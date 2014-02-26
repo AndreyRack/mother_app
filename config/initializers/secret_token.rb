e9213344d9747ec97ae4e68df145087d0ba3f03c
Andrey Rack <andrey.rack95@gmail.com>
2014-02-26 10:13:41 +0200

    Create project mother_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MotherApp::Application.config.secret_token = '0d20ea284c87cf313d73d78d51e31540993303746e29467fb9f4a4faf91be4c1806d73c50310f1e160fc5b455eb618ebb858a9fce0a71d36239b9fd794dc1086'
